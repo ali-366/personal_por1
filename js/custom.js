3916b89720008fb99145bcd78512eb2cabb7cc42
HOSSIAN MOHAMMOD ALI <you@example.com>
2022-03-21 23:58:55 +0900

    30%ready

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  
+    $('.sk_1').circleGraphic();
+    $('.sk_2').circleGraphic();
+    $('.sk_3').circleGraphic();
+    $('.sk_4').circleGraphic();
+    $('.sk_5').circleGraphic();
+    $('.sk_6').circleGraphic();
+    $('.sk_7').circleGraphic();
+    $('.sk_8').circleGraphic();
+    
+ });
